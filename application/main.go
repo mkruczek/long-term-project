f39b09f33bf8e0c69e45d1fa969bd346cd391e3d
kruczek <kruczek86@gmail.com>
2023-08-03 18:38:36 +0200

    cleanup old code

---
+++ application/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"net/http"
+	"time"
+)
+
+var mongoClient *mongo.Client
+
+func health(w http.ResponseWriter, req *http.Request) {
+
+	if err := mongoClient.Ping(context.Background(), nil); err != nil {
+		http.Error(w, "unable to ping mongodb", http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "i am alive\n")
+}
+
+func main() {
+
+	http.HandleFunc("/api/health", health)
+
+	fmt.Println("starting server on port 8090")
+	if err := http.ListenAndServe(":8090", nil); err != nil {
+		panic(fmt.Sprintf("unable to start server: %v", err))
+	}
+}
+
+func init() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	var err error
+	mongoClient, err = mongo.Connect(ctx, options.Client().ApplyURI("mongodb://root:secret@localhost:27017"))
+	if err != nil {
+		panic(fmt.Sprintf("unable to connect to mongodb: %v", err))
+	}
+}
