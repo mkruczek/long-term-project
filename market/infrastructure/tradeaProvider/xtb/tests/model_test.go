d1c70d8ae7507a9affa20521ec328f66b6e46621
kruczek <kruczek86@gmail.com>
2023-08-28 07:07:43 +0200

    add case for pip profit/lose and run test parallel

--- market/infrastructure/tradeaProvider/xtb/tests/model_test.go
+++ market/infrastructure/tradeaProvider/xtb/tests/model_test.go
@@ -109,10 +109,107 @@ func Test_ConvertXtbCsvToDomainModel(t *testing.T) {
 				ExternalID: "12345678",
 			},
 		},
+		{name: "EURUSD-Buy-1PipProfit",
+			xtbCsv: xtb.CSV{
+				Position:   "12345678",
+				Symbol:     "EURUSD",
+				Type:       "Buy",
+				OpenTime:   "2020-01-01 00:00:00",
+				OpenPrice:  1.00005,
+				CloseTime:  "2020-01-01 00:00:00",
+				ClosePrice: 1.00015,
+				Profit:     10, //for single pip profit
+				NetProfit:  10,
+			},
+			expected: domain.Trade{
+				ID:         "12345678",
+				Symbol:     "EURUSD",
+				TradeSide:  domain.Buy,
+				OpenPrice:  domain.Price{Value: 1.00005, Coefficient: 100000},
+				OpenTime:   time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
+				ClosePrice: domain.Price{Value: 1.00015, Coefficient: 100000},
+				CloseTime:  time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
+				Profit:     10,
+				ExternalID: "12345678",
+			},
+		},
+		{name: "EURUSD-Buy-1PipLos",
+			xtbCsv: xtb.CSV{
+				Position:   "12345678",
+				Symbol:     "EURUSD",
+				Type:       "Buy",
+				OpenTime:   "2020-01-01 00:00:00",
+				OpenPrice:  1.00015,
+				CloseTime:  "2020-01-01 00:00:00",
+				ClosePrice: 1.00005,
+				Profit:     -10, //for single pip profit
+				NetProfit:  -10,
+			},
+			expected: domain.Trade{
+				ID:         "12345678",
+				Symbol:     "EURUSD",
+				TradeSide:  domain.Buy,
+				OpenPrice:  domain.Price{Value: 1.00015, Coefficient: 100000},
+				OpenTime:   time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
+				ClosePrice: domain.Price{Value: 1.00005, Coefficient: 100000},
+				CloseTime:  time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
+				Profit:     -10,
+				ExternalID: "12345678",
+			},
+		},
+		{name: "EURJPY-Buy-1PipProfit",
+			xtbCsv: xtb.CSV{
+				Position:   "12345678",
+				Symbol:     "EURJPY",
+				Type:       "Buy",
+				OpenTime:   "2020-01-01 00:00:00",
+				OpenPrice:  123.005,
+				CloseTime:  "2020-01-01 00:00:00",
+				ClosePrice: 123.015,
+				Profit:     10, //for single pip profit
+				NetProfit:  10,
+			},
+			expected: domain.Trade{
+				ID:         "12345678",
+				Symbol:     "EURJPY",
+				TradeSide:  domain.Buy,
+				OpenPrice:  domain.Price{Value: 123.005, Coefficient: 1000},
+				OpenTime:   time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
+				ClosePrice: domain.Price{Value: 123.015, Coefficient: 1000},
+				CloseTime:  time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
+				Profit:     10,
+				ExternalID: "12345678",
+			},
+		},
+		{name: "EURJPY-Buy-1PipLos",
+			xtbCsv: xtb.CSV{
+				Position:   "12345678",
+				Symbol:     "EURJPY",
+				Type:       "Buy",
+				OpenTime:   "2020-01-01 00:00:00",
+				OpenPrice:  123.015,
+				CloseTime:  "2020-01-01 00:00:00",
+				ClosePrice: 123.005,
+				Profit:     -10, //for single pip profit
+				NetProfit:  -10,
+			},
+			expected: domain.Trade{
+				ID:         "12345678",
+				Symbol:     "EURJPY",
+				TradeSide:  domain.Buy,
+				OpenPrice:  domain.Price{Value: 123.015, Coefficient: 1000},
+				OpenTime:   time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
+				ClosePrice: domain.Price{Value: 123.005, Coefficient: 1000},
+				CloseTime:  time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
+				Profit:     -10,
+				ExternalID: "12345678",
+			},
+		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
 			actual, err := tc.xtbCsv.ToDomainModel()
 			if err != nil {
 				t.Errorf("unexpected error: %s", err)
