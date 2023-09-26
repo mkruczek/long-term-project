fcbeb9950cb7b9a67a138546b1206b06818e76f6
kruczek <kruczek86@gmail.com>
2023-09-26 18:55:44 +0200

    playing with go image library

--- market/application/handlers/image.go
+++ market/application/handlers/image.go
@@ -8,29 +8,31 @@ import (
 	"image/png"
 	"market/market/application/service"
 	"os"
+	"time"
 )
 
-type bar struct {
-	Open  int
-	Close int
-	High  int
-	Low   int
-	//todo add time
+var (
+	backgroundColor = color.RGBA{R: 50, G: 50, B: 50, A: 255}
+)
+
+// chart will be created from two axis: time - X and price - Y
+// chart will be a background for bars
+type chart struct {
+	bars []bar
+	Xs   []time.Time // range of time from earliest -2 to latest +2, will be converted to int
+	Ys   []int       // range of prices from lowest -2 to highest +2
 }
 
-// values for bar in points
-func fakeData() []bar {
-	return []bar{
-		{Open: 100100, Close: 100400, High: 100500, Low: 100050},
-		{Open: 100400, Close: 100100, High: 100600, Low: 100000},
-
-		{Open: 107188, Close: 107188, High: 107208, Low: 107078},
-		{Open: 107188, Close: 107188, High: 107208, Low: 107078},
+func newChart(bars []bar) chart {
+	return chart{
+		bars: bars,
+		Xs:   []time.Time{bars[0].Time.Add(-2 * time.Minute), bars[len(bars)-1].Time.Add(2 * time.Minute)},
+		Ys:   []int{bars[0].Low - 2, bars[len(bars)-1].High + 2},
 	}
 }
 
 /*
-Single bar view
+bar is a single bar in chart
 example view is a pixel
 ---**---
 ---**---
@@ -42,6 +44,24 @@ example view is a pixel
 ---**---
 ---**---
 */
+type bar struct {
+	Open  int
+	Close int
+	High  int
+	Low   int
+	Time  time.Time
+}
+
+// values for bar in points, assume that i will get one minute bar
+func fakeData() []bar {
+	return []bar{
+		{Open: 100100, Close: 100400, High: 100500, Low: 100050, Time: time.Now()},
+		{Open: 100400, Close: 100100, High: 100600, Low: 100000, Time: time.Now().Add(time.Minute)},
+		{Open: 100100, Close: 100200, High: 100400, Low: 100000, Time: time.Now().Add(time.Minute * 2)},
+		{Open: 100200, Close: 100400, High: 100400, Low: 100200, Time: time.Now().Add(time.Minute * 3)},
+	}
+}
+
 func createSingleBarView(b bar) image.Image {
 	width := 8
 	height := b.High - b.Low
@@ -68,13 +88,13 @@ func createSingleBarView(b bar) image.Image {
 	//make transparent all pixels above open and to 3 pixel right
 	for x := 0; x < 3; x++ {
 		for y := 0; y < o.Y; y++ {
-			img.Set(x, y, color.Transparent)
+			img.Set(x, y, backgroundColor)
 		}
 	}
 	//make transparent all pixels below open and to 3 pixel right
 	for x := 0; x < 3; x++ {
 		for y := o.Y + 3; y < height; y++ {
-			img.Set(x, y, color.Transparent)
+			img.Set(x, y, backgroundColor)
 		}
 	}
 
@@ -83,14 +103,14 @@ func createSingleBarView(b bar) image.Image {
 	//make transparent all pixels above close and to 3 pixel left
 	for x := width - 3; x < width; x++ {
 		for y := 0; y < c.Y; y++ {
-			img.Set(x, y, color.Transparent)
+			img.Set(x, y, backgroundColor)
 		}
 	}
 
 	//make transparent all pixels below close and to 3 pixel left
 	for x := width - 3; x < width; x++ {
 		for y := c.Y + 3; y < height; y++ {
-			img.Set(x, y, color.Transparent)
+			img.Set(x, y, backgroundColor)
 		}
 	}
 
@@ -101,23 +121,42 @@ func CreateImage(trd service.Trades) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		//ctx := c.Request.Context()
 
-		fd := fakeData()
+		ch := newChart(fakeData())
+
+		//calculate width base on time
+		imgWidth := 8*len(ch.bars) + 2*8 // 8 pixels for one bar and 8 pixels for each side, first bar will be 8 pixels from left side
 
 		//create image
-		img := image.NewRGBA(image.Rect(0, 0, 8*len(fd), 1000))
+		img := image.NewRGBA(image.Rect(0, 0, imgWidth, ch.Ys[1]-ch.Ys[0]))
 		// set background color
-		for x := 0; x < 8*len(fd); x++ {
-			for y := 0; y < 100; y++ {
-				img.Set(x, y, color.RGBA{R: 0, G: 0, B: 255, A: 255}) // white color {R:255, G:255, B:255, A:255}
+		for x := 0; x < 8*len(ch.bars); x++ {
+			for y := 0; y < ch.Ys[1]-ch.Ys[0]; y++ {
+				img.Set(x, y, backgroundColor)
 			}
 		}
 
 		//draw bars
-		for i, b := range fd {
+		for i, b := range ch.bars {
 			drawAt := image.Point{X: 8 * i, Y: 0}
-			drawRect := image.Rectangle{Min: drawAt, Max: drawAt.Add(image.Point{X: 8, Y: 1000})}
+			drawRect := image.Rectangle{Min: drawAt, Max: drawAt.Add(image.Point{X: 8, Y: ch.Ys[1] - ch.Ys[0]})}
 			draw.Draw(img, drawRect, createSingleBarView(b), image.Point{}, draw.Src)
 		}
+
+		////create image
+		//img := image.NewRGBA(image.Rect(0, 0, 1000, 1000))
+		//// set background color
+		//for x := 0; x < 8*len(fd); x++ {
+		//	for y := 0; y < 100; y++ {
+		//		img.Set(x, y, color.RGBA{R: 0, G: 0, B: 255, A: 255}) // white color {R:255, G:255, B:255, A:255}
+		//	}
+		//}
+		//
+		////draw bars
+		//for i, b := range fd {
+		//	drawAt := image.Point{X: 8 * i, Y: 0}
+		//	drawRect := image.Rectangle{Min: drawAt, Max: drawAt.Add(image.Point{X: 8, Y: 1000})}
+		//	draw.Draw(img, drawRect, createSingleBarView(b), image.Point{}, draw.Src)
+		//}
 
 		// Encode as PNG.
 		f, _ := os.Create("image.png")
